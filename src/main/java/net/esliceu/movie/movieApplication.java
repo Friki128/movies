279efad11301eda4e806dc81864ad5a18a620e1e
Zelda <zeldagarrocho@gmail.com>
2025-01-20 15:37:16 +0100

    First Commit

---
+++ src/main/java/net/esliceu/movie/movieApplication.java
@@ -0,0 +1,13 @@
+package net.esliceu.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class movieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(movieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/esliceu/movie/movieApplicationTests.java
@@ -0,0 +1,13 @@
+package net.esliceu.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class movieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
